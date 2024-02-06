679e56fc94d900567b470ea2e41162d459b3d531
Yanxiang Zhan <yanxiang@uni-bremen.de>
2024-02-06 12:29:34 +0100

    update background process management

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -3,4 +3,8 @@ source ${ROS_WS}/devel/setup.bash
 roscore &
 roslaunch --wait rvizweb rvizweb.launch &
 
+MUJOCO_WORLD_PATH=${ROS_WS}/src/mujoco_world/mujoco_world
+mkdir ${MUJOCO_WORLD_PATH}/mujoco_world
+ln -s ${MUJOCO_WORLD_PATH}/model ${MUJOCO_WORLD_PATH}/mujoco_world/model
+
 exec "$@"
