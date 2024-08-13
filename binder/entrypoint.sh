727e48a2ca77b34ea601a41fc041acd279d72a21
malte <huerkamp@uni-bremen.de>
2024-08-13 15:52:34 +0200

    entrypoint and rviz config

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source ${ROS_WS}/devel/setup.bash
 roscore &
-roslaunch --wait rvizweb rvizweb.launch interactive_markers:=True &
+roslaunch --wait rvizweb rvizweb.launch config_file:=launch/rvizweb_config/pr2.json &
+roslaunch --wait giskardpy giskardpy_pr2_standalone_vrb.launch &
 
 exec "$@"
