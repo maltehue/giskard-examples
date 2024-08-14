7f5d7da3f71cd5321a20eea3aee325ba5c5f88eb
malte <huerkamp@uni-bremen.de>
2024-08-14 09:42:23 +0200

    added iai_maps and upload kitchen

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -3,5 +3,6 @@ source ${ROS_WS}/devel/setup.bash
 roscore &
 roslaunch --wait rvizweb rvizweb.launch config_file:=launch/rvizweb_config/pr2.json &
 roslaunch --wait giskardpy giskardpy_pr2_standalone_vrb.launch &
+roslaunch --wait iai_maps upload_kitchen_obj.launch &
 
 exec "$@"
