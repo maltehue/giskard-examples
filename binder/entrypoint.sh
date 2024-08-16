791c0443a98c287941342a3b9660ea96ac29bc65
malte <huerkamp@uni-bremen.de>
2024-08-16 11:47:37 +0200

    pip install for launch from notebook

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -2,7 +2,7 @@
 source ${ROS_WS}/devel/setup.bash
 roscore &
 roslaunch --wait rvizweb rvizweb.launch config_file:=launch/rvizweb_config/pr2.json &
-roslaunch --wait giskardpy giskardpy_pr2_standalone_vrb.launch &
+# roslaunch --wait giskardpy giskardpy_pr2_standalone_vrb.launch &
 roslaunch --wait iai_kitchen upload_kitchen_obj.launch &
 
 exec "$@"
