651ab0e053ddf5ef9c9c1710571349579fa41323
malte <huerkamp@uni-bremen.de>
2024-08-19 10:36:38 +0200

    pouring script

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ${ROS_WS}/devel/setup.bash
 roscore &
-roslaunch --wait rvizweb rvizweb.launch &
+#roslaunch --wait rvizweb rvizweb.launch &
 
 MUJOCO_WORLD_PATH=${ROS_WS}/src/mujoco_world/mujoco_world
 mkdir ${MUJOCO_WORLD_PATH}/mujoco_world
