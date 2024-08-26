199eeaff51bca807d5e8d5fc01ec5dcf3d434f6f
malte <huerkamp@uni-bremen.de>
2024-08-26 15:44:06 +0200

    rvizweb config

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ${ROS_WS}/devel/setup.bash
 roscore &
-roslaunch --wait rvizweb rvizweb.launch config_file:=launch/rvizweb_config/hsr_mujoco.json &
+roslaunch --wait rvizweb rvizweb.launch config_file:=launch/rvizweb_config/pr2_mujoco.json &
 
 jupyter lab workspaces import binder/vis-with-terminal.jupyterlab-workspace
 
