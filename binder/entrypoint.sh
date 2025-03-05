aa320756f7c58acbdcfe127972ef6d4cb6b63448
malte <huerkamp@uni-bremen.de>
2025-03-05 17:14:03 +0100

    bug

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ${ROS_WS}/devel/setup.bash
 roscore &
-
+roslaunch --wait rvizweb rvizweb.launch config_file:=launch/rvizweb_config/hsr_mujoco.json &
 roslaunch --wait dlr_kitchen upload_dlr_kitchen.launch &
 roslaunch --wait giskardpy_ros giskardpy_pr2_standalone_vrb.launch &
 rviz -d /home/jovyan/giskard_examples/launch/rvizweb_config/bmp.rviz &
