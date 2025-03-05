7dfad2ed40389581671371ee2cb540e8e7a7fe8b
malte <huerkamp@uni-bremen.de>
2025-03-05 13:25:43 +0100

    launch kitchen on startup

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -2,6 +2,7 @@
 source ${ROS_WS}/devel/setup.bash
 roscore &
 roslaunch --wait rvizweb rvizweb.launch config_file:=launch/rvizweb_config/hsr_mujoco.json &
+roslaunch --wait dlr_kitchen upload_dlr_kitchen.launch &
 
 jupyter lab workspaces import binder/vis-with-terminal.jupyterlab-workspace
 
