1650f9d89842f6e05abd230cadd8b7e942c065f8
malte <huerkamp@uni-bremen.de>
2025-03-07 16:05:55 +0100

    entrypoint remove pr2 launch

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -3,7 +3,7 @@ source ${ROS_WS}/devel/setup.bash
 roscore &
 roslaunch --wait rvizweb rvizweb.launch config_file:=launch/rvizweb_config/hsr_mujoco.json &
 roslaunch --wait dlr_kitchen upload_dlr_kitchen.launch &
-roslaunch --wait giskardpy_ros giskardpy_pr2_standalone_vrb.launch &
+
 rviz -d /home/jovyan/giskard_examples/launch/rvizweb_config/bmp.rviz &
 
 jupyter lab workspaces import binder/vis-with-terminal.jupyterlab-workspace
