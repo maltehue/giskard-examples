c96365c5baf124000e8137cd900ecfc83a4f28ed
maltehue <huerkamp@uni-bremen.de>
2025-06-13 22:20:41 +0200

    update

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -3,6 +3,7 @@ source ${ROS_WS}/devel/setup.bash
 roscore &
 roslaunch --wait dlr_kitchen upload_dlr_kitchen.launch &
 roslaunch --wait giskardpy_ros giskardpy_pr2_standalone_vrb.launch &
+roslaunch --wait rvizweb rvizweb.launch config_file:=launch/rvizweb_config/hsr_mujoco.json &
 
 
 jupyter lab workspaces import binder/vis-with-terminal.jupyterlab-workspace
